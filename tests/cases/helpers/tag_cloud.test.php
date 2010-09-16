94ce0787d5b3da5cdfd978342494fc94f4673839
Phize <info@phize.net>
2010-09-16 18:41:20 +0900

    add TagCloud Helper.

---
+++ tests/cases/helpers/tag_cloud.test.php
@@ -0,0 +1,185 @@
+<?php
+App::import('Controller', 'Controller', false);
+App::import('Helper', 'TagCloud');
+
+class TestTagCloudHelper extends TagCloudHelper {
+	public function test_prune($tags, $minScore = 1) {
+		return $this->_prune($tags, $minScore);
+	}
+
+	public function test_sort($tags, $direction = 'desc') {
+		return $this->_sort($tags, $direction);
+	}
+
+	public function test_filter($tags, $limit = null) {
+		return $this->_filter($tags, $limit);
+	}
+
+	public function test_calculateRate($tags) {
+		return $this->_calculateRate($tags);
+	}
+}
+
+class TagCloudHelperTestCase extends CakeTestCase {
+	public function start() {
+		parent::start();
+	}
+
+	public function end() {
+		parent::end();
+	}
+
+	public function startCase() {
+		parent::startCase();
+	}
+
+	public function endCase() {
+		parent::endCase();
+	}
+
+	public function startTest($method) {
+		parent::startTest($method);
+		$this->TagCloud =& new TestTagCloudHelper();
+	}
+
+	public function endTest($method) {
+		parent::endTest($method);
+		unset($this->TagCloud);
+	}
+
+	/**
+	 * _prune()のテスト
+	 */
+	public function test_prune() {
+		$tags = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag4' => 9);
+		$result = $this->TagCloud->test_prune($tags);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+		$result = $this->TagCloud->test_prune($tags, 0);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10);
+		$result = $this->TagCloud->test_prune($tags, 10);
+		$this->assertIdentical($result, $expected);
+	}
+
+	/**
+	 * _sort()のテスト
+	 */
+	public function test_sort() {
+		$tags = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+
+		$expected = array('tag1' => 10, 'tag4' => 9, 'tag2' => 1, 'tag3' => 0);
+		$result = $this->TagCloud->test_sort($tags);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+		$result = $this->TagCloud->test_sort($tags, null);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+		$result = $this->TagCloud->test_sort($tags, 'unknown');
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag3' => 0, 'tag2' => 1, 'tag4' => 9, 'tag1' => 10);
+		$result = $this->TagCloud->test_sort($tags, 'asc');
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag4' => 9, 'tag2' => 1, 'tag3' => 0);
+		$result = $this->TagCloud->test_sort($tags, 'desc');
+		$this->assertIdentical($result, $expected);
+	}
+
+	/**
+	 * _filter()のテスト
+	 */
+	public function test_filter() {
+		$tags = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+		$result = $this->TagCloud->test_filter($tags);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag2' => 1);
+		$result = $this->TagCloud->test_filter($tags, 2);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+		$result = $this->TagCloud->test_filter($tags, 10);
+		$this->assertIdentical($result, $expected);
+	}
+
+	/**
+	 * _calculateRate()のテスト
+	 */
+	public function test_calculateRate() {
+		$tags = array('tag1' => 10, 'tag2' => 10, 'tag3' => 10, 'tag4' => 10);
+		$expected = array(
+			'tag1' => 0.5,
+			'tag2' => 0.5,
+			'tag3' => 0.5,
+			'tag4' => 0.5
+		);
+		$result = $this->TagCloud->test_calculateRate($tags);
+		$this->assertIdentical($result, $expected);
+	}
+
+	/**
+	 * calculate()のテスト
+	 */
+	public function testCalculate() {
+		$tags = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+
+		$expected = array(
+			'tag1' => array('score' => 10, 'rank' => 25),
+			'tag2' => array('score' => 1, 'rank' => 1),
+			'tag4' => array('score' => 9, 'rank' => 23)
+		);
+		$result = $this->TagCloud->calculate($tags);
+		$this->assertIdentical($result, $expected);
+
+		$expected = array(
+			'tag1' => array('score' => 10, 'rank' => 250),
+			'tag2' => array('score' => 1, 'rank' => 10),
+			'tag4' => array('score' => 9, 'rank' => 232)
+		);
+		$result = $this->TagCloud->calculate($tags, array('min' => 10, 'max' => 250));
+		$this->assertIdentical($result, $expected);
+
+		$expected = array(
+			'tag1' => array('score' => 10, 'rank' => 25),
+			'tag2' => array('score' => 1, 'rank' => 9),
+			'tag3' => array('score' => 0, 'rank' => 1),
+			'tag4' => array('score' => 9, 'rank' => 24)
+		);
+		$result = $this->TagCloud->calculate($tags, array('threshold' => 0));
+		$this->assertIdentical($result, $expected);
+
+		$expected = array(
+			'tag1' => array('score' => 10, 'rank' => 25),
+			'tag4' => array('score' => 9, 'rank' => 23),
+			'tag2' => array('score' => 1, 'rank' => 1)
+		);
+		$result = $this->TagCloud->calculate($tags, array('sort' => 'desc'));
+		$this->assertIdentical($result, $expected);
+
+		$expected = array(
+			'tag1' => array('score' => 10, 'rank' => 13)
+		);
+		$result = $this->TagCloud->calculate($tags, array('limit' => 1));
+		$this->assertIdentical($result, $expected);
+	}
+
+	/**
+	 * shuffle()のテスト
+	 */
+	public function testShuffle() {
+		$tags = array('tag1' => 10, 'tag2' => 1, 'tag3' => 0, 'tag4' => 9);
+
+		$result = $this->TagCloud->shuffle($tags);
+		$this->assertNotIdentical($result, $tags);
+	}
+}
---
+++ views/helpers/tag_cloud.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * タグクラウドヘルパー
+ *
+ * 以下の形式の連想配列からタグクラウドを生成
+ * array(
+ *		// タグ => タグ数
+ *		'tag1' => 100,
+ *		'tag2' => 200
+ * )
+ */
+class TagCloudHelper extends Helper {
+	/**
+	 * タグのランクを算出
+	 *
+	 * 結果として以下の形式の連想配列を返す。
+	 * array(
+	 *		// タグ => array(
+	 *		//	'score' => タグ数,
+	 *		//	'rank' => タグのランク,
+	 *		// )
+	 *		'tag1' => array(
+	 *			'score' => 100,
+	 *			'rank' => 1
+	 *		),
+	 *		'tag2' => array(
+	 *			'score' => 200,
+	 *			'rank' => 25
+	 *		)
+	 * )
+	 *
+	 * 以下のオプションを指定することができる。
+	 * min			タグランクの最小値
+	 * max			タグランクの最大値
+	 * threshold	タグ件数の閾値 (指定した件数未満のタグが除去される)
+	 * sort			タグの並び替え方向 ('asc' = 昇順, 'desc' = 降順)
+	 * limit		タグの件数
+	 *
+	 * @param array $tags タグのデータ
+	 * @param array $options オプション
+	 * @return array 算出後のデータ
+	 */
+	public function calculate($tags, $options = array()) {
+		$defaults = array(
+			'min' => 1,
+			'max' => 25,
+			'threshold' => 1,
+			'sort' => null,
+			'limit' => null
+		);
+		$options = array_merge($defaults, $options);
+
+		if ($options['threshold'] !== null) $tags = $this->_prune($tags, $options['threshold']);
+		if ($options['sort'] !== null) $tags = $this->_sort($tags, $options['sort']);
+		if ($options['limit'] !== null) $tags = $this->_filter($tags, $options['limit']);
+
+		$rates = $this->_calculateRate($tags);
+
+		$newTags = array();
+		foreach ($rates as $tag => $rate) {
+			$newTags[$tag]['score'] = $tags[$tag];
+			$newTags[$tag]['rank'] = (int) round($rate * ($options['max'] - $options['min']) + $options['min']);
+		}
+
+		return $newTags;
+	}
+
+	/**
+	 * タグを剪定
+	 *
+	 * 最小スコア未満のタグを除去する。
+	 *
+	 * @param array $tags タグのデータ
+	 * @param integer $minScore 最小スコア
+	 * @return array 剪定後のデータ
+	 */
+	protected function _prune($tags, $minScore = 1) {
+		foreach ($tags as $tag => $score) {
+			if ($score < $minScore) unset($tags[$tag]);
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * タグをスコアで並び替え
+	 *
+	 * @param array $tags タグのデータ
+	 * @param string $direction 並び替えの方向 ('asc' = 昇順, 'desc' = 降順)
+	 * @return array 並び替え後のデータ
+	 */
+	protected function _sort($tags, $direction = 'desc') {
+		if ($direction === null) return $tags;
+		$direction = strtolower($direction);
+
+		switch ($direction) {
+			case 'asc':
+				asort($tags);
+				break;
+			case 'desc':
+				arsort($tags);
+				break;
+			default:
+				break;
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * タグの件数を限定
+	 *
+	 * @param array $tags タグのデータ
+	 * @param integer $limit 件数
+	 * @return array 限定後のデータ
+	 */
+	protected function _filter($tags, $limit = null) {
+		if ($limit === null) return $tags;
+
+		$_tags = $tags;
+		$tags = array_slice($tags, 0, $limit);
+		$tags = array_intersect_key($_tags, $tags);
+
+		return $tags;
+	}
+
+	/**
+	 * タグのレートを算出
+	 *
+	 * @param array $tags タグのデータ
+	 * @return array 算出後のデータ
+	 */
+	protected function _calculateRate($tags) {
+		$max = max($tags);
+		$min = min($tags);
+
+		if ($max - $min == 0) {
+			foreach ($tags as $tag => $score) {
+				$tags[$tag] = 0.5;
+			}
+
+			return $tags;
+		}
+
+		$sqrtMax = sqrt($max);
+		$sqrtMin = sqrt($min);
+
+		foreach ($tags as $tag => $score) {
+			$sqrtScore = sqrt($score);
+			$tags[$tag] = ($sqrtScore - $sqrtMin) / ($sqrtMax - $sqrtMin);
+		}
+
+		return $tags;
+	}
+
+	/**
+	 * タグクラウドをシャッフル
+	 *
+	 * @param array $tags タグのデータ
+	 * @return array シャッフル後のデータ
+	 */
+	public function shuffle($tags) {
+		$newTags = array();
+		while (count($tags) > 0) {
+			$tag = array_rand($tags);
+			$newTags[$tag] = $tags[$tag];
+			unset($tags[$tag]);
+		}
+
+		return $newTags;
+	}
+}
